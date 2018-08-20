0f9bfd074c59c228ace7048870d4f390fbd0a234
Nicholas Kostelnik <nitaigao@users.noreply.github.com>
2018-08-20 17:05:51 +0800

    guid

---
+++ src/AssemblyClient/Model/Subject.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace AssemblyClient {
+
+	public class Subject
+	{
+		[JsonProperty("id")]
+		public int? Id { get; set; }
+
+		[JsonProperty("code")]
+		public string Code { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+	}
+}
