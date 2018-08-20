e073f2334551ace2cca524b200d05034709d3106
Nicholas Kostelnik <nitaigao@users.noreply.github.com>
2018-08-20 16:00:35 +0800

    update sln file

---
+++ src/AssemblyClient/Resources/Subjects.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Dynamic;
+using System.Threading.Tasks;
+
+namespace AssemblyClient
+{
+	public class Subjects : Resource
+	{
+		public Subjects(ApiClient client)
+			: base(client)
+		{
+
+		}
+
+		public async Task<List<Subject>> Index(
+		  int? perPage = null, 
+		  int? page = null
+		)
+		{
+			dynamic args = new ExpandoObject();
+			args.perPage = perPage;
+			args.page = page;
+
+			var results = await Client.GetList<Subject>("/subjects", args);
+
+			return results;
+		}
+
+	}
+
+	public partial class ApiClient
+	{
+		public Subjects Subjects => new Subjects(this);
+	}
+}
